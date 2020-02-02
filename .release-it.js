5fe0471b8869334f9ed0f9917f3f01df5a30185a
wulunyi <wulunyi@meituan.com>
2020-02-02 10:45:45 +0800

    chore(master): 引入 release-it

---
+++ .release-it.js
@@ -0,0 +1,7 @@
+module.exports = {
+    hooks: {
+        'before:init': 'npm run compile',
+        'after:release':
+            'echo Successfully released ${name} v${version} to ${repo.repository}.',
+    },
+};
